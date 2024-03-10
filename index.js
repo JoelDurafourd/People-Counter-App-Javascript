cf8547a4c9e2e8d17bf7754fa35ebcbeb3c23dfb
JoelDurafourd <joel.durafourd@gmail.com>
2024-03-10 15:19:35 +0900

    Added button, added background, started writing scripts

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-document.getElementById("count-el").innerText = 5
+//document.getElementById("count-el").innerText = 5
+
+let count = 0
+
+console.log(count)
+
+function increment() {
+    count = count +1
+}
+
+let lap1 = 34 
+let lap2 = 33
+let lap3 = 36
